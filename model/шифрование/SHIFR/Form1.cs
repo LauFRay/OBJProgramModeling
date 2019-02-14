d96f6ff795a896aacdb563edf69903cda34bdcd7
LauFRay <LauFRay@users.noreply.github.com>
2019-02-14 23:44:55 +0600

    gg

--- model/шифрование/SHIFR/Form1.cs
+++ model/шифрование/SHIFR/Form1.cs
@@ -12,7 +12,7 @@ namespace SHIFR
 {
     public partial class Form1 : Form
     {
-        private char[] alfavit = { 'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я'//, '.', '-', ',', ' ' 
+        public char[] alfavit = { 'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я'//, '.', '-', ',', ' ' 
         };
    
         public Form1()
@@ -20,43 +20,7 @@ namespace SHIFR
             InitializeComponent();
         }
         
-        private string Cezar(string sentence,int number)
-        {
-           sentence = sentence.ToUpper();
-
-            string result = string.Empty;
-  
-            for (int i = 0; i < sentence.Length; i++)
-            {
-                for(int j = 0; j < alfavit.Length; j++)
-                {
-                    if (alfavit[j].Equals(sentence[i]))
-                    {
-                        if ((j + number) >= 0)
-                        {
-                            Math.DivRem(j + number, alfavit.Length, out int IndexOfWord);
-                            result += alfavit.ElementAt(IndexOfWord);
-                            break;
-                        }
-                        else
-                        {
-                            result += alfavit.ElementAt(alfavit.Length + number);
-                            break;
-                        }
-                            
-                    }
-                    else if(sentence[i].Equals(' '))
-                    {
-                        result += " ";
-                        break;
-                    }
-                }
-
-               
-            }
-
-            return result;
-        }
+        
 
         private string Newshifr(string sentence)
         {
@@ -89,7 +53,7 @@ namespace SHIFR
             switch(comboBox1.SelectedIndex)
             {
                 case 0:
-                    label1.Text = Cezar(textBox1.Text, Convert.ToInt32(numericUpDown1.Value));
+                    label1.Text = Cezar(textBox1.Text, Convert.ToInt32(ereere));
                     break;
                 case 1:
                     label1.Text = Newshifr(textBox1.Text);
@@ -100,7 +64,7 @@ namespace SHIFR
 
         private void button2_Click(object sender, EventArgs e)
         {
-            label1.Text = Cezar(textBox1.Text, Convert.ToInt32(-numericUpDown1.Value));
+            label1.Text = Cezar(textBox1.Text, - Convert.ToInt32(rereer));
         }
     }
 }
