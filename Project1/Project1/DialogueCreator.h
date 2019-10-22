0ad30146040ffeee8651b8ad0221371db100a7d2
LauFRay <LauFRay@users.noreply.github.com>
2019-10-22 11:43:29 +0600

    Creeper Aw man

--- Project1/Project1/DialogueCreator.h
+++ Project1/Project1/DialogueCreator.h
@@ -34,6 +34,10 @@ namespace Project1 {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Button^  button1;
+	protected:
+	private: System::Windows::Forms::Button^  button2;
+	private: System::Windows::Forms::Button^  button3;
 
 	private:
 		/// <summary>
@@ -48,12 +52,61 @@ namespace Project1 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"DialogueCreator";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(12, 12);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(119, 52);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"Create Dialogue";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &DialogueCreator::button1_Click);
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(12, 70);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(119, 50);
+			this->button2->TabIndex = 1;
+			this->button2->Text = L"Edit Dialogue";
+			this->button2->UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this->button3->Location = System::Drawing::Point(12, 126);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(119, 25);
+			this->button3->TabIndex = 2;
+			this->button3->Text = L"Exit";
+			this->button3->UseVisualStyleBackColor = true;
+			// 
+			// DialogueCreator
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->AutoSize = true;
+			this->ClientSize = System::Drawing::Size(376, 320);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
+			this->Name = L"DialogueCreator";
+			this->Text = L"DialogueCreator";
+			this->Load += gcnew System::EventHandler(this, &DialogueCreator::DialogueCreator_Load);
+			this->ResumeLayout(false);
+
 		}
 #pragma endregion
+	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+	}
+			 
+
+	private: System::Void DialogueCreator_Load(System::Object^  sender, System::EventArgs^  e) {
+	}
+
 	};
 }
