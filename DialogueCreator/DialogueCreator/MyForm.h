4d45b8200eaf947c4933e51438afc2f0271fad5a
LauFRay <LauFRay@users.noreply.github.com>
2019-10-29 11:22:37 +0600

    Got our pickaxe swinging from side to side

--- DialogueCreator/DialogueCreator/MyForm.h
+++ DialogueCreator/DialogueCreator/MyForm.h
@@ -34,6 +34,16 @@ namespace DialogueCreator {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::OpenFileDialog^  OFD;
+	private: System::Windows::Forms::SaveFileDialog^  SFD;
+	protected:
+
+	protected:
+
+	private: System::Windows::Forms::Button^  btnCreate;
+	private: System::Windows::Forms::Button^  btnEdit;
+
+	protected:
 
 	private:
 		/// <summary>
@@ -48,12 +58,54 @@ namespace DialogueCreator {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"MyForm";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->OFD = (gcnew System::Windows::Forms::OpenFileDialog());
+			this->SFD = (gcnew System::Windows::Forms::SaveFileDialog());
+			this->btnCreate = (gcnew System::Windows::Forms::Button());
+			this->btnEdit = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// SFD
+			// 
+			this->SFD->DefaultExt = L"xml";
+			// 
+			// btnCreate
+			// 
+			this->btnCreate->Location = System::Drawing::Point(12, 12);
+			this->btnCreate->Name = L"btnCreate";
+			this->btnCreate->Size = System::Drawing::Size(75, 23);
+			this->btnCreate->TabIndex = 0;
+			this->btnCreate->Text = L"CREATE";
+			this->btnCreate->UseVisualStyleBackColor = true;
+			this->btnCreate->Click += gcnew System::EventHandler(this, &MyForm::btnCreate_Click);
+			// 
+			// btnEdit
+			// 
+			this->btnEdit->Location = System::Drawing::Point(12, 41);
+			this->btnEdit->Name = L"btnEdit";
+			this->btnEdit->Size = System::Drawing::Size(75, 23);
+			this->btnEdit->TabIndex = 1;
+			this->btnEdit->Text = L"EDIT";
+			this->btnEdit->UseVisualStyleBackColor = true;
+			this->btnEdit->Click += gcnew System::EventHandler(this, &MyForm::btnEdit_Click);
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(820, 261);
+			this->Controls->Add(this->btnEdit);
+			this->Controls->Add(this->btnCreate);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->ResumeLayout(false);
+
 		}
 #pragma endregion
+	private: System::Void btnCreate_Click(System::Object^  sender, System::EventArgs^  e) {
+		SFD->ShowDialog();
+	}
+	private: System::Void btnEdit_Click(System::Object^  sender, System::EventArgs^  e) {
+		OFD->ShowDialog();
+	}
 	};
 }
