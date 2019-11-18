fcf93bd24edeb692362ff4858975ae56c26cfc76
LauFRay <LauFRay@users.noreply.github.com>
2019-11-19 01:30:01 +0600

    This task, a grueling one Hope to find some diamonds tonight, night, night Diamonds tonight

---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkedListBox1.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.checkedListBox1.Location = new System.Drawing.Point(12, 137);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(773, 301);
+            this.checkedListBox1.TabIndex = 0;
+            this.checkedListBox1.TabStop = false;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(773, 91);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "fewsdfsdfsdfsd";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(797, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.CheckedListBox checkedListBox1;
+    }
+}
+
---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string[,] sTest = new  string[200,6];
+        int[] value = new int[200];
+        int q = 0;
+        int question = 0;
+
+        int[] arr; 
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load("C://Users/Acer/Documents/test/XMLFile1.xml");
+            XmlElement xRoot = xDoc.DocumentElement;
+            foreach (XmlNode xnode in xRoot)
+            {
+                if (xnode.Attributes.Count > 0)
+                {
+                    XmlNode attr = xnode.Attributes.GetNamedItem("name");
+                    if (attr != null) sTest[q,0]= attr.Value;
+                }
+                foreach (XmlNode childnode in xnode.ChildNodes)
+                {
+                    if (childnode.Name == "v1") { sTest[q, 1] = childnode.InnerText; }
+                    if (childnode.Name == "v2") { sTest[q, 2] = childnode.InnerText; }
+                    if (childnode.Name == "v3") { sTest[q, 3] = childnode.InnerText; }
+                    if (childnode.Name == "v4") { sTest[q, 4] = childnode.InnerText; }
+                    if (childnode.Name == "v5") { sTest[q, 5] = childnode.InnerText; }
+                }
+                q++;
+            }
+            
+
+            int[] array = new int[q];
+            int number = 0;
+            int saveInt = 0;
+
+            Random random = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = i ;
+            }
+            for (int a = 0; a < array.Length; a++)
+            {
+                while (true)
+                {
+                    number = random.Next(0, array.Length);
+                    if (a <= number)
+                    {
+                        saveInt = array[a];
+                        array[a] = array[number];
+                        array[number] = saveInt;
+                        break;
+                    }
+                }
+            }
+            arr = array;
+
+            label1.Text = " ";
+            checkedListBox1.Items.Clear();
+
+            label1.Text = sTest[array[0], 0];
+            for (int j = 1; j <= 5; j++)
+            {
+                if (sTest[array[question], j] != null)
+                {
+                    checkedListBox1.Items.Add(sTest[array[question], j]);
+                }
+            }
+            //for (int i = 0; i< array.Length;i++)
+            //{
+            //    label1.Text += array[i].ToString();
+            //    label1.Text += " ";
+            //}
+
+
+
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (checkedListBox1.CheckedItems.Count > 0)
+            {
+                for (int i = 0; i < checkedListBox1.Items.Count; i++)
+                    checkedListBox1.SetItemChecked(i, false);
+                checkedListBox1.SetItemChecked(checkedListBox1.SelectedIndex, true);
+                if (question < arr.Length)
+                question++;
+                label1.Text = " ";
+                checkedListBox1.Items.Clear();
+
+                label1.Text = sTest[arr[0], 0];
+                for (int j = 1; j <= 5; j++)
+                {
+                    if (sTest[arr[question], j] != null)
+                    {
+                        checkedListBox1.Items.Add(sTest[arr[question], j]);
+                    }
+                }
+            }
+        }
+    }
+}
